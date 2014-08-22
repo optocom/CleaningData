1607416fe23bafe828168eded4166a0c78f53711
Hsing <optocom@users.noreply.github.com>
2014-08-22 03:06:32 -0700

    Signed-off-by: Hsing <optocom@users.noreply.github.com>

--- run_analysis.R
+++ run_analysis.R
@@ -13,8 +13,8 @@ allDir <- "all"
 if (!file.exists(allDir)) dir.create(allDir)
 
 # Get all variables in text file features
-features <- unlist(strsplit(readLines("features.txt"),split=" "))  # unlist to make a vector
-features <- features[c(seq(from=2, to=length(features), by=2))]  # pick the names only
+features <- read.table("features.txt", colClasses=c("integer","character"))
+features <- features[,2]  # features: character vector (not factor since colClasses is specified)
 
 # Read various files
 subject_test <- read.table("test/subject_test.txt",sep="\t")
@@ -35,28 +35,32 @@ write.table(subject,"all/subject.txt",row.names=FALSE)
 write.table(activity,"all/activity.txt",row.names=FALSE)
 
 # Read the Test data file
-x <- readLines("test/X_test.txt")
-n <- length(x)  # number of obs. 2947L
-x <- unlist(strsplit(x,split=" "))  # extract numbers from a string
-x <- as.numeric(x[nchar(x[1:length(x)])!=0])  # remove empty string
-dim(x) <- c(n,length(x)/n)  # convert to matrix
-x_test <- as.data.frame(x)  # convert to data.frame
-names(x_test) <- features  # add column names
+x_test <- read.table("test/X_test.txt", colClasses="numeric")
+## if readLines is used as below, it requires more processing.
+## x <- readLines("test/X_test.txt")
+## n <- length(x)  # number of obs. 2947L
+## x <- unlist(strsplit(x,split=" "))  # extract numbers from a string
+## x <- as.numeric(x[nchar(x[1:length(x)])!=0])  # remove empty string
+## dim(x) <- c(n,length(x)/n)  # convert to matrix
+## x_test <- as.data.frame(x)  # convert to data.frame
 
 # Read the Train data file
-x <- readLines("train/X_train.txt")
-n <- length(x)  # number of obs. 7352L
-x <- unlist(strsplit(x,split=" "))  # extract numbers from a string
-x <- as.numeric(x[nchar(x[1:length(x)])!=0])  # remove empty string
-dim(x) <- c(n,length(x)/n)  # convert to matrix
-x_train <- as.data.frame(x)  # convert to data.frame
-names(x_train) <- features  # add column names
+x_train <- read.table("train/X_train.txt", colClasses="numeric")
+## if readLines is used as below, it requires more processing.
+## x <- readLines("train/X_train.txt")
+## n <- length(x)  # number of obs. 7352L
+## x <- unlist(strsplit(x,split=" "))  # extract numbers from a string
+## x <- as.numeric(x[nchar(x[1:length(x)])!=0])  # remove empty string
+## dim(x) <- c(n,length(x)/n)  # convert to matrix
+## x_train <- as.data.frame(x)  # convert to data.frame
 
 x <- rbind(x_train,x_test)  # bind train and test X
+names(x) <- features  # add column names
 write.table(x,"all/X.txt",sep="\t",row.name=FALSE)
+rm(x_test, x_train)  # remove variables
 
 # The following is to combine the Train and Test data in folder Inertial Signals
-### It is not needed for this project so I added 'if (FALSE)' block to skip them
+### It isn't needed for this project so I added 'if (FALSE)' block to skip them
 if (FALSE)
 { ### start of block ###
 dirTest <- "test/Inertial Signals/"
@@ -76,20 +80,10 @@ for (counter in 1:nFiles) {
     fileName <- paste(fileName,".txt",sep="")  # add .txt
 
     # Read the data from the Test file
-    x_test <- readLines(paste(dirTest,fileTest[counter],sep=""))
-    n <- length(x_test)
-    x_test <- unlist(strsplit(x_test,split=" "))
-    x_test <- as.numeric(x_test[nchar(x_test[1:length(x_test)])!=0])
-    dim(x_test) <- c(n,length(x_test)/n)  # convert to matrix
-    x_test <- as.data.frame(x_test)  # convert to data.frame
+    x_test <- read.table(paste(dirTest,fileTest[counter],sep=""), colClasses="numeric")
 
     # Read the data from the Train file
-    x_train <- readLines(paste(dirTrain,fileTrain[counter],sep=""))
-    n <- length(x_train)
-    x_train <- unlist(strsplit(x_train,split=" "))
-    x_train <- as.numeric(x_train[nchar(x_train[1:length(x_train)])!=0])
-    dim(x_train) <- c(n,length(x_train)/n)
-    x_train <- as.data.frame(x_train)
+    x_train <- read.table(paste(dirTrain,fileTrain[counter],sep=""), colClasses="numeric")
 
     # Combine two data frames
     y <- rbind(x_train,x_test)
@@ -98,31 +92,35 @@ for (counter in 1:nFiles) {
     write.table(y,file=paste(dirAll,fileName,sep="/"),sep="\t",row.name=FALSE)
 }
 # Remove unnecessary variables
-rm(list=c("x_train","x_test","fileTest","fileTrain","n","y","fileName"))
+rm(list=c("fileTest","fileTrain","y","fileName","counter","nFiles"))
 
 } ### end of skip block ###
 
 # Find the columns with mean and standard deviation in features
 found <- grep("mean|std",features)  # including meanFreq()
-  # found <- grep("mean[(]|std",features)  # not including meanFreq()
+## found <- grep("mean[(]|std",features)  # not including meanFreq()
 
 # Extract the mean and std data from x to xFound
 xFound <- x[,found]  # Extract the data from x to xFound
 names(xFound) <- features[found]  # Add the names to the columns
 # Combine the activity, the subject and the data into one dataset xFound
 xFound <- cbind(activity=as.character(activity_label[activity[,1],2]),subject=subject,xFound)
+# Save the variables to a text file for CodeBook
+write.table(cbind(1L:length(found), data.frame(features[found])), file="features_avg.txt", row.names=FALSE, col.names=FALSE)
+# Remove unnecessary variables
+rm(x,activity,subject,activity_label)
 
 # Load the package to make tidy data
 library(reshape2)
 
 # Use melt to form a tidy dataset
-xFoundMelt <- melt(xFound,id.vars=c("activity","subject"),measure.vars=features[found])
+xFoundMelt <- melt(xFound, id.vars=c("activity","subject"), measure.vars=features[found])
 
 # Use dcast to get the average of each variable by each activity and each subject
 avg <- dcast(xFoundMelt, activity + subject ~ variable, mean)
 
 # Use melt to form a tidy dataset of the average
-avgMelt <- melt(avg,id=c("activity","subject"),measure.vars=features[found])
+avgMelt <- melt(avg,id=c("activity","subject"), measure.vars=features[found])
 
 # Write the output to a file "average.txt" without row.name, using default sep
-write.table(avgMelt,file="average.txt",row.name=FALSE)
+write.table(avgMelt, file="average.txt", row.name=FALSE)
